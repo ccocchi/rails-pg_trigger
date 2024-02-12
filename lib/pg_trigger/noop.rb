c22ce0ee87d2c3c78f2ed8f0a1f52664320b9488
ccocchi <cocchi.c@gmail.com>
2024-02-12 12:07:34 +0100

    Fix noop

--- lib/pg_trigger/noop.rb
+++ lib/pg_trigger/noop.rb
@@ -3,8 +3,23 @@ module PgTrigger::Noop
     other.extend ClassMethods
   end
 
+  class Proxy
+    def self.chain(*methods)
+      methods.each do |m|
+        class_eval <<-RUBY, __FILE__, __LINE__ + 1
+          def #{m}(*)
+            self
+          end
+        RUBY
+      end
+    end
+
+    chain :on, :of, :after, :before, :named, :where, :nowrap
+  end
+
   module ClassMethods
     def trigger
+      Proxy.new
     end
   end
 end
